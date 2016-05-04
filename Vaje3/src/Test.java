54d2dab814a8e5f18da61bb773248dbb9d13455f
bbYYJnhA <jure.bernot@student.fmf.uni-lj.si>
2016-05-04 15:31:02 +0200

    Popravil merge confilkt

--- Vaje3/src/Test.java
+++ Vaje3/src/Test.java
@@ -2,7 +2,6 @@
 public class Test {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		Graf f = new Graf();
 		// popravi napako
 		//f.povezan();
@@ -15,11 +14,7 @@ public class Test {
 		f.dodajTocko(z);
 		Graf g = Graf.cikel(10);
 		Graf h = Graf.poln(10);		
-=======
-		Graf g = Graf.cikel(42);
 		g.razporedi(300, 300, 250);
-		Graf h = Graf.poln(5);
->>>>>>> 163ac0c9dc2402b292982a394cdc409d0ee492f9
 		//h.poln(5);
 		System.out.println(f);
 		System.out.println(g);
