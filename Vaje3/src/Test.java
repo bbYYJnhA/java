163ac0c9dc2402b292982a394cdc409d0ee492f9
bbYYfaks <bbYYJnhA@bitbucked.org>
2016-05-04 14:05:44 +0200

    na 4vajah

--- Vaje3/src/Test.java
+++ Vaje3/src/Test.java
@@ -2,12 +2,17 @@
 public class Test {
 
 	public static void main(String[] args) {
-		Graf g = Graf.cikel(5);
+		Graf g = Graf.cikel(42);
+		g.razporedi(300, 300, 250);
 		Graf h = Graf.poln(5);
 		//h.poln(5);
 		System.out.println(g);
 		System.out.println(h);
 		System.out.println("Konec programa");
+		Okno okno = new Okno();
+		okno.pack();
+		okno.setVisible(true);
+		okno.platno.narisi(g);
 
 	}
 
